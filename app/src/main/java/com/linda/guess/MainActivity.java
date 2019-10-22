2a9431025c214da4a69111c8f53374aee662584a
lincindiawang <lindawang0207@gmail.com>
2019-10-22 16:54:19 +0800

    2nd commit

--- app/src/main/java/com/linda/guess/MainActivity.java
+++ app/src/main/java/com/linda/guess/MainActivity.java
@@ -8,11 +8,23 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    int guessTime;
+
+    String TAG = MainActivity.class.getSimpleName();
+    private EditText number;
+
+    int secret = new Random().nextInt(10) +1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +32,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        number = findViewById( R.id.ed_num);
+
+
+
+        Log.d(TAG,"Secret : "+ secret);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
 
+    public void Guess(View view){
+        guessTime +=1;
+        TextView inform = findViewById(R.id.inform);
+        TextView times = findViewById(R.id.times);
+        int num = Integer.parseInt(String.valueOf(number.getText().toString()));
+
+        if (num > secret){
+            inform.setText("Smaller!!");
+            //Toast.makeText(MainActivity.this,"Smaller !!",Toast.LENGTH_LONG).show();
+            //num = 0;
+
+        }else if (num < secret){
+            inform.setText("Bigger!!");
+            //Toast.makeText(MainActivity.this,"Bigger !!",Toast.LENGTH_LONG).show();
+            //num = 0;
+        }else {
+            inform.setText("You Got It ! The number is : " + secret);
+            //Toast.makeText(MainActivity.this,"You Got It ! The number is :" + secret,Toast.LENGTH_LONG).show();
+
+        }
+        times.setText("猜了" + guessTime +"次");
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
